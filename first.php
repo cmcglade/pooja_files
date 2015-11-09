034349de75133ca01e3b7345d11fbe7ef87514fe
Pooja Patel <poojapatel934@gmial.com>
2015-11-08 21:37:59 -0500

    Added first php file.

---
+++ first.php
@@ -0,0 +1,22 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+<?php
+$x="Destine";
+echo "Hello Friends.";
+echo "<br>";
+echo $x;
+echo "<br>";
+for($i=0;$i<=5;$i++)
+{
+	echo $x;
+}
+?>
+<br />
+Now HTML starts.
+</body>
+</html>
