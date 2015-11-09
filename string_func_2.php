42c0d14446d35dace173a3409f9c294ed42a83e9
Pooja Patel <poojapatel934@gmial.com>
2015-11-08 21:45:58 -0500

    Added second practice php for string functions.

---
+++ string_func_2.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+<?php
+$x="Destine";
+strtolower($x);
+echo "$x is now lower case";
+echo "<br>";
+strtoupper($x)
+echo "$x is now upper case";
+echo "<br>";
+?>
+
+</body>
+</html>
